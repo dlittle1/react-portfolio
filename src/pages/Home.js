34d2240505953ee3148b6a8667e95e83e24bb932
Dylan Little <dylan.c.little@gmail.com>
2022-04-07 23:25:32 -0600

    add tooltips to development icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,18 @@ import '../styles/home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCss3Alt } from '@fortawesome/free-brands-svg-icons';
 import { faGit } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faNode } from '@fortawesome/free-brands-svg-icons';
+import { faJava } from '@fortawesome/free-brands-svg-icons';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
 import { faJsSquare } from '@fortawesome/free-brands-svg-icons';
 import { faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import ReactTooltip from 'react-tooltip';
 
 const Home = (props) => {
   const handleClick = () => {
     props.projectsRef.current.scrollIntoView();
   };
+
   return (
     <div className='home'>
       <div id='home' ref={props.refProp}></div>
@@ -30,15 +33,103 @@ const Home = (props) => {
             View Projects
           </button>
           <div className='home-icons'>
-            <FontAwesomeIcon icon={faGit} size='3x' />
-
-            <FontAwesomeIcon icon={faReact} size='3x' />
-
-            <FontAwesomeIcon icon={faJsSquare} size='3x' />
-
-            <FontAwesomeIcon icon={faHtml5} size='3x' />
-
-            <FontAwesomeIcon icon={faCss3Alt} size='3x' />
+            <div className='home-icon-container'>
+              <FontAwesomeIcon
+                icon={faGit}
+                size='3x'
+                data-tip
+                data-for='gitIcon'
+              />
+              <ReactTooltip id='gitIcon' place='top' type='dark' effect='float'>
+                <p>git</p>
+              </ReactTooltip>
+            </div>
+            <div className='home-icon-container'>
+              <FontAwesomeIcon
+                icon={faJava}
+                size='3x'
+                data-tip
+                data-for='javaIcon'
+              />
+              <ReactTooltip
+                id='javaIcon'
+                place='top'
+                type='dark'
+                effect='float'
+              >
+                <p>Java</p>
+              </ReactTooltip>
+            </div>
+            <div className='home-icon-container'>
+              <FontAwesomeIcon
+                icon={faReact}
+                size='3x'
+                data-tip
+                data-for='reactIcon'
+              />
+              <ReactTooltip
+                id='reactIcon'
+                place='top'
+                type='dark'
+                effect='float'
+              >
+                <p>React</p>
+              </ReactTooltip>
+            </div>
+            <div className='home-icon-container'>
+              <FontAwesomeIcon
+                icon={faNode}
+                size='3x'
+                data-tip
+                data-for='nodeIcon'
+              />
+              <ReactTooltip
+                id='nodeIcon'
+                place='top'
+                type='dark'
+                effect='float'
+              >
+                <p>Node</p>
+              </ReactTooltip>
+            </div>
+            <div className='home-icon-container'>
+              <FontAwesomeIcon
+                icon={faJsSquare}
+                size='3x'
+                data-tip
+                data-for='jsIcon'
+              />
+              <ReactTooltip id='jsIcon' place='top' type='dark' effect='float'>
+                <p>JavaScript</p>
+              </ReactTooltip>
+            </div>
+            <div className='home-icon-container'>
+              <FontAwesomeIcon
+                icon={faHtml5}
+                size='3x'
+                data-tip
+                data-for='htmlIcon'
+              />
+              <ReactTooltip
+                id='htmlIcon'
+                place='top'
+                type='dark'
+                effect='float'
+              >
+                <p>HTML</p>
+              </ReactTooltip>
+            </div>
+            <div className='home-icon-container'>
+              <FontAwesomeIcon
+                icon={faCss3Alt}
+                size='3x'
+                data-tip
+                data-for='cssIcon'
+              />
+              <ReactTooltip id='cssIcon' place='top' type='dark' effect='float'>
+                <p>CSS</p>
+              </ReactTooltip>
+            </div>
           </div>
         </div>
       </div>
