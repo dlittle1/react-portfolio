3b82ee63c37f64529e5a490f19fd4d93293df4f1
Dylan Little <dylan.c.little@gmail.com>
2022-04-08 00:40:24 -0600

    refactor projects page to use css

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,48 +1,31 @@
-import React, { useRef } from 'react';
-import RickAndMorty from '../assets/projectScreenshots/RickAndMortyHome.PNG';
-import QuizApp from '../assets/projectScreenshots/QuizApp.PNG';
-import { Box, Typography, Grid, Card } from '@mui/material';
+import React from 'react';
 import data from '../projectData.js';
 import { Link } from 'react-router-dom';
+import '../styles/projects.css';
 const Projects = (props) => {
-  const bracketColor = { color: '#BC5980' };
   console.log(data);
   return (
-    <Box sx={{ marginBottom: { xs: '1rem', md: '20rem' } }}>
-      <Box id='projects' ref={props.refProp}>
-        <Typography
-          textAlign='center'
-          variant='h1'
-          mb={3}
-          xs={{ fontSize: { xs: '2rem', sm: '3rem', lg: '4.5rem' } }}
-        >
-          Projects
-        </Typography>
-      </Box>
-      <Grid container spacing={4} padding={3}>
+    <div className='projects'>
+      <div id='projects' ref={props.refProp}>
+        <h1>Projects</h1>
+      </div>
+      <div className='projects-container'>
         {data.map((project, index) => (
-          <Grid
-            component={Link}
-            to={`/projects/${index}`}
-            item
-            xs={12}
-            md={6}
-            key={index}
-            sx={{ textDecoration: 'none' }}
-          >
-            <Card>
-              <Typography variant='h5' sx={{ padding: '20px' }}>
-                {project.title}
-              </Typography>
-              <img
-                src={project.imgs[0]}
-                style={{ maxWidth: '100%', float: 'right' }}
-              />
-            </Card>
-          </Grid>
+          <Link to={`/projects/${index}`}>
+            <div className='projects-project-container'>
+              <div key={project.title + index}>
+                <h5>{project.title}</h5>
+                <img
+                  src={project.imgs[0]}
+                  style={{ maxWidth: '100%', float: 'right' }}
+                  alt={project.title}
+                />
+              </div>
+            </div>
+          </Link>
         ))}
-      </Grid>
-    </Box>
+      </div>
+    </div>
   );
 };
 
