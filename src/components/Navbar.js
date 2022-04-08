f02ba2b36c560a31c2d4d264c1e906b8dfb00347
Dylan Little <dylan.c.little@gmail.com>
2022-04-08 00:39:49 -0600

    refactor Navbar to use css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,47 @@
-import { TabsContext } from '@mui/base'
-import { AppBar, Toolbar, Tab, Tabs } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import '../styles/navbar.css';
 const Navbar = (props) => {
-  const [value, setValue] = useState('1')
+  const [value, setValue] = useState('1');
 
   const handleChange = (e, newValue) => {
-    setValue(newValue)
+    setValue(newValue);
 
     if (newValue === '1') {
-      props.homeRefProp.current.scrollIntoView({ block: 'center' })
+      props.homeRefProp.current.scrollIntoView({ block: 'center' });
     } else if (newValue === '2') {
-      const projectsElem = props.projectsRefProp.current
-      const yOffset = -60
+      const projectsElem = props.projectsRefProp.current;
+      const yOffset = -60;
       const y =
-        projectsElem.getBoundingClientRect().top + window.pageYOffset + yOffset
-      window.scrollTo({ top: y, behavior: 'smooth' })
+        projectsElem.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
     } else {
-      props.aboutRefProp.current.scrollIntoView()
+      props.aboutRefProp.current.scrollIntoView();
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', checkViewport)
+    window.addEventListener('scroll', checkViewport);
 
     return () => {
-      window.removeEventListener('scroll', checkViewport)
-    }
-  })
-
-  const tabMargin = { marginRight: { xs: '0px', sm: '15px', md: '40px' } }
-  const tabsGroup = {
-    marginLeft: { xs: '0px', sm: '30px', md: '60px' },
-    marginTop: '15px',
-    position: 'fixed',
-  }
+      window.removeEventListener('scroll', checkViewport);
+    };
+  });
 
   function checkViewport(e) {
-    const homePage = document.querySelector('#home')
-    const projectsPage = document.querySelector('#projects')
-    const aboutPage = document.querySelector('#about')
-    const rectHome = homePage.getBoundingClientRect()
-    const rectProjects = projectsPage.getBoundingClientRect()
-    const rectAbout = aboutPage.getBoundingClientRect()
-
-    if (isInViewport(rectAbout)) {
-      console.log('about')
-    }
+    const homePage = document.querySelector('#home');
+    const projectsPage = document.querySelector('#projects');
+    const aboutPage = document.querySelector('#about');
+    const rectHome = homePage.getBoundingClientRect();
+    const rectProjects = projectsPage.getBoundingClientRect();
+    const rectAbout = aboutPage.getBoundingClientRect();
 
     if (isInViewport(rectHome)) {
-      setValue('1')
+      setValue('1');
     } else if (isInViewport(rectProjects)) {
-      setValue('2')
+      setValue('2');
     } else if (isInViewport(rectAbout)) {
-      setValue('3')
+      setValue('3');
     }
   }
 
@@ -64,33 +52,23 @@ const Navbar = (props) => {
       rect.bottom <=
         (window.innerHeight || document.documentElement.clientHeight) &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    )
+    );
   }
 
   return (
     <>
-      <AppBar sx={{ backgroundColor: 'white' }}>
-        <Toolbar sx={{ zIndex: 105 }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            textColor='secondary'
-            TabIndicatorProps={{
-              style: {
-                display: 'none',
-              },
-            }}
-            aria-label='tabs'
-            sx={tabsGroup}
-          >
-            <Tab label='Home' value='1' sx={tabMargin} />
-            <Tab label='Projects' value='2' sx={tabMargin} />
-            <Tab label='About Me' value='3' sx={tabMargin} />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
+      <nav className='nav'>
+        <ul className='nav-items'>
+          <li className='nav-item'>Home</li>
+          <li className='nav-item' onClick={(e) => handleChange(e, '2')}>
+            Projects
+          </li>
+          <li className='nav-item'>About Me</li>
+        </ul>
+      </nav>
+      <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
