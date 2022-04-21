75b9ffc0d6d253789f6d1bc3fff13e8962e9afa1
Dylan Little <dylan.c.little@gmail.com>
2022-04-21 00:30:56 -0600

    remove unused icon packages

--- src/components/ProjectsNavbar.js
+++ src/components/ProjectsNavbar.js
@@ -3,7 +3,7 @@ import NavItem from './NavItem';
 import projectsData from '../data/projectData';
 import './component-styles/projectsNavbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/pro-duotone-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 const ProjectsNavbar = ({ handleDrawer }) => {
   return (
     <nav className='projects-nav'>
