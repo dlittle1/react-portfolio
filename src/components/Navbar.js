c5ae095a05c0532da3277bff3ca495670ebcccfd
Dylan Little <dylan.c.little@gmail.com>
2022-04-20 21:24:41 -0600

    add social links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,84 +2,81 @@ import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import '../styles/navbar.css';
 import NavItem from './NavItem';
+import {
+  faGithub,
+  faLinkedin,
+  faStackOverflow,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 const Navbar = (props) => {
   const [value, setValue] = useState('1');
 
-  // const handleChange = (e, newValue) => {
-  //   setValue(newValue);
-
-  //   if (newValue === '1') {
-  //     props.homeRefProp.current.scrollIntoView({ block: 'center' });
-  //   } else if (newValue === '2') {
-  //     const projectsElem = props.projectsRefProp.current;
-  //     const yOffset = -60;
-  //     const y =
-  //       projectsElem.getBoundingClientRect().top + window.pageYOffset + yOffset;
-  //     window.scrollTo({ top: y, behavior: 'smooth' });
-  //   } else {
-  //     props.aboutRefProp.current.scrollIntoView();
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', checkViewport);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', checkViewport);
-  //   };
-  // });
-
-  // function checkViewport(e) {
-  //   const homePage = props.homeRefProp.current;
-  //   const projectsPage = props.projectsRefProp.current;
-  //   const aboutPage = props.aboutRefProp.current;
-  //   const rectHome = homePage.getBoundingClientRect();
-  //   const rectProjects = projectsPage.getBoundingClientRect();
-  //   const rectAbout = aboutPage.getBoundingClientRect();
-
-  //   if (isInViewport(rectHome)) {
-  //     setValue('1');
-  //   } else if (isInViewport(rectProjects)) {
-  //     setValue('2');
-  //   } else if (isInViewport(rectAbout)) {
-  //     setValue('3');
-  //   }
-  // }
-
-  // function isInViewport(rect) {
-  //   return (
-  //     console.log(rect.top, rect.bottom, rect.left, rect.right),
-  //     rect.top >= 0 &&
-  //       rect.left >= -1 &&
-  //       rect.bottom <=
-  //         (window.innerHeight || document.documentElement.clientHeight) &&
-  //       rect.right <=
-  //         (window.innerWidth || document.documentElement.clientWidth)
-  //   );
-  // }
+  const socialNavLinks = [
+    {
+      name: 'Github',
+      link: 'https://github.com/dlittle1',
+      icon: faGithub,
+      tooltip: 'Github',
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/dylanlittle/',
+      icon: faLinkedin,
+      tooltip: 'LinkedIn',
+    },
+    {
+      name: 'Stack Overflow',
+      link: 'https://stackoverflow.com/users/4192210/dylan-little',
+      icon: faStackOverflow,
+      tooltip: 'Stack Overflow',
+    },
+    {
+      name: 'Resume',
+      link: 'https://github.com/dlittle1/resume/raw/main/Dylan%20Resume%20v3%20.pdf',
+      icon: faFileAlt,
+      tooltip: 'Resume',
+    },
+  ];
 
   return (
     <>
       <nav className='projects-nav'>
         <ul className='nav-items' style={{ width: props.width }}>
-          <NavItem
-            title='Home'
-            value='1'
-            linkTo={'home'}
-            classname={value === '1' ? 'active' : ''}
-          />
-          <NavItem
-            title='Projects'
-            value='2'
-            linkTo={'projects'}
-            classname={value === '2' ? 'active' : ''}
-          />
-          <NavItem
-            title='About'
-            value='3'
-            linkTo={'about'}
-            classname={value === '3' ? 'active' : ''}
-          />
+          <div className='nav-links-main'>
+            <NavItem
+              title='Home'
+              value='1'
+              linkTo={'home'}
+              classname={value === '1' ? 'active' : ''}
+            />
+            <NavItem
+              title='Projects'
+              value='2'
+              linkTo={'projects'}
+              classname={value === '2' ? 'active' : ''}
+            />
+            <NavItem
+              title='About'
+              value='3'
+              linkTo={'about'}
+              classname={value === '3' ? 'active' : ''}
+            />
+          </div>
+          <div className='nav-links-social'>
+            {socialNavLinks.map((link, index) => (
+              <li key={link + index}>
+                <a
+                  href={link.link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='social-nav-item'
+                >
+                  <FontAwesomeIcon icon={link.icon} size='2x' />
+                </a>
+              </li>
+            ))}
+          </div>
         </ul>
       </nav>
       <Outlet />
