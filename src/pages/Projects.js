3c068e5d5b3edda1a9afd22cf7ae7330d43f42e4
Dylan Little <dylan.c.little@gmail.com>
2022-04-26 00:30:56 -0600

    try loading content

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import projectData from '../data/projectData.js';
 import { Link } from 'react-router-dom';
 import '../styles/projects.css';
 import Masonry from 'react-masonry-css';
 
 const Projects = (props) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   const breakpoints = {
     default: 4,
     1700: 3,
@@ -20,37 +28,47 @@ const Projects = (props) => {
     });
   };
 
-  return (
-    <div className='projects'>
-      <div id='projects' className='projects-heading' ref={props.refProp}>
-        <h1>Projects</h1>
-        <p>(click project for more info)</p>
+  if (loading) {
+    return (
+      <div className='loading-container'>
+        <div className='loading-text'>
+          <h1>Loading...</h1>
+        </div>
       </div>
-      <Masonry
-        breakpointCols={breakpoints}
-        className='my-masonry-grid'
-        columnClassName='my-masonry-grid_column'
-      >
-        {projectData.map((project, index) => (
-          <div
-            className='projects-project-container'
-            key={project.title + index}
-          >
-            <Link to={`/projects/${index}`} onClick={scrollToTop}>
-              <div key={project.title + index}>
-                <h5>{project.title}</h5>
-              </div>
-              <img
-                src={project.imgs[0].src}
-                style={{ maxWidth: '100%', float: 'right' }}
-                alt={project.title}
-              />
-            </Link>
-          </div>
-        ))}
-      </Masonry>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className='projects'>
+        <div id='projects' className='projects-heading' ref={props.refProp}>
+          <h1>Projects</h1>
+          <p>(click project for more info)</p>
+        </div>
+        <Masonry
+          breakpointCols={breakpoints}
+          className='my-masonry-grid'
+          columnClassName='my-masonry-grid_column'
+        >
+          {projectData.map((project, index) => (
+            <div
+              className='projects-project-container'
+              key={project.title + index}
+            >
+              <Link to={`/projects/${index}`} onClick={scrollToTop}>
+                <div key={project.title + index}>
+                  <h5>{project.title}</h5>
+                </div>
+                <img
+                  src={project.imgs[0].src}
+                  style={{ maxWidth: '100%', float: 'right' }}
+                  alt={project.title}
+                />
+              </Link>
+            </div>
+          ))}
+        </Masonry>
+      </div>
+    );
+  }
 };
 
 export default Projects;
