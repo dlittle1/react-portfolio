a6c991db2ece1d2b4fb0c1af111f8fe62e437615
Dylan Little <dylan.c.little@gmail.com>
2022-05-18 17:13:16 -0600

    weird fix for tooltips

--- src/components/DevIcons.js
+++ src/components/DevIcons.js
@@ -4,28 +4,50 @@ import devIconsData from '../data/devIconsData';
 import ReactTooltip from 'react-tooltip';
 
 const DevIcons = () => {
+  const [tooltip, showTooltip] = React.useState(true);
+
   return (
     <div className='home-icons'>
       {devIconsData.map((icon, index) => (
         <div key={icon.name + index} className='home-icon-container'>
           {icon.name === 'Redux' ? (
-            <div className='home-icon-redux' data-tip data-for={icon.tooltip}>
+            <div
+              className='home-icon-redux'
+              data-tip
+              data-for={icon.tooltip}
+              onMouseEnter={() => showTooltip(true)}
+              onMouseLeave={() => {
+                showTooltip(false);
+                setTimeout(() => {
+                  showTooltip(true);
+                }, 50);
+              }}
+            >
               <i className='fak fa-redux fa-3x' />
             </div>
           ) : (
-            <div className='home-icon' data-tip data-for={icon.tooltip}>
-              <FontAwesomeIcon icon={icon.icon} size='3x' />
-            </div>
+            <>
+              <div
+                className='home-icon'
+                data-tip
+                data-for={icon.tooltip}
+                onMouseEnter={() => showTooltip(true)}
+                onMouseLeave={() => {
+                  showTooltip(false);
+                  setTimeout(() => {
+                    showTooltip(true);
+                  }, 50);
+                }}
+              >
+                <FontAwesomeIcon icon={icon.icon} size='3x' />
+              </div>
+            </>
           )}
-
-          <ReactTooltip
-            id={icon.tooltip}
-            place='top'
-            type='dark'
-            effect='float'
-          >
-            {icon.tooltip}
-          </ReactTooltip>
+          {tooltip && (
+            <ReactTooltip id={icon.tooltip} type='dark' effect='float'>
+              {icon.tooltip}
+            </ReactTooltip>
+          )}
         </div>
       ))}
     </div>
